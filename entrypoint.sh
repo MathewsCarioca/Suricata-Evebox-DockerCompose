94af58682a07d00936eb9dc76c582731475f9122
Matheus Monteiro <monteiromatheus596@gmail.com>
2024-07-31 08:57:56 -0300

    Update Real Oficial

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,6 @@
 suricata -c /etc/suricata/suricata.yaml -r /var/log/suricata/pcap.pcap -v -l /var/log/suricata &
 #suricata -c /etc/suricata/suricata.yaml -i eth0 &
 
-#sleep 5 &
 
 #suricatasc -c reload-rules -v
 
